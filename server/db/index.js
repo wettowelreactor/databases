21270d139c3fb8c7bf6b97e9565992559c653ff6
wettowelreactor <camille@rousselfamily.com>
2015-02-27 16:13:54 -0800

    Inital SQL connection working, schema working.

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,21 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'chat'
+});
+connection.connect();
+
+exports.db = connection;
+
+
+exports.db.query('Select * from rooms;', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows);
+});
+
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,34 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE tweet (
+  ID int NOT NULL auto_increment,
+  roomID int,
+  userID int,
+  msg varchar(140),
+  time timestamp default current_timestamp,
+  PRIMARY KEY (ID)
 );
+CREATE TABLE users (
+  ID int NOT NULL auto_increment,
+  roomID int,
+  name varchar(16) default 'anon',
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE rooms (
+  ID int NOT NULL auto_increment,
+  name varchar(16) default 'lobby',
+  PRIMARY KEY (ID)
+);
+
+INSERT INTO users (roomID, name) values (1, 'bob'), (1, 'alice'), (2, 'rick');
+INSERT INTO users (roomID) values (1);
+INSERT INTO rooms () values ();
+INSERT INTO rooms (name) values ('secret Room'), ('8th Floor');
+INSERT INTO tweet (roomID, userID, msg) values (1, 1, 'This is bob'), (1, 2, 'this is alice'), (2, 1, 'Bob has left'), (1, 4, 'spy');
 
 /* Create other tables and define schemas for them here! */
 
